55292169f2eb6ca9deded4f04be6f2fd94d6e63e
Margaret Ochieng <margaret.ochieng@andela.com>
2016-09-13 11:52:41 +0300

    [Chore] rename folder to mainpic

---
+++ mainpic/pimppic/settings/production.py
@@ -0,0 +1,18 @@
+"""
+Production specific settings.
+"""
+
+from .base import *
+import dj_database_url
+
+
+DEBUG = False
+
+DATABASES = {
+    'default': dj_database_url.config()
+}
+
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+ALLOWED_HOSTS = ['*']
