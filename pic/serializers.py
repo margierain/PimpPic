593b4ea0ba14c075e6cfc5819bd2595ab407e029
Margaret Ochieng <margaret.ochieng@andela.com>
2016-09-22 12:40:02 +0300

    [Chore] add model serializers

---
+++ pic/serializers.py
@@ -0,0 +1,35 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+from .models import TimeStampMixin, Folder, Image
+from rest_framework.serializers import (
+    ModelSerializer,
+)
+
+
+class TimeStampMixinSerializer(ModelSerializer):
+    pass
+
+    class Meta:
+        model = TimeStampMixin
+        fields = ('date_created', 'date_modified')
+        read_only_fields = ('date_created')
+
+
+class ImageSerializer(TimeStampMixinSerializer):
+    pass
+
+    class Meta:
+        model = Folder
+        fields = ('id', 'image', 'image_thumbnail', 'edited_image', 'title',
+                  'effects', 'share_image', 'uploader', 'folder')
+        read_only_fields = ('uploader', 'id')
+
+
+class FolderSerializer(TimeStampMixinSerializer):
+    images = ImageSerializer(many=True, read_only=True)
+    creator = serializers.ReadOnlyField(source='creator.id')
+
+    class Meta:
+        model = Folder
+        fields = ('id', 'name', 'creator')
+        read_only_fields = ('creator', 'id',)
